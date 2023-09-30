67041febffe044b498b2e634f93b84273d3eaa97
Andrej Orsula <orsula.andrej@gmail.com>
2023-09-30 20:26:25 +0200

    Update config and add new hyperparameter presets
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- drl_air_hockey/utils/config.py
+++ drl_air_hockey/utils/config.py
@@ -1,33 +1,31 @@
-from os import cpu_count, environ, path
+from os import environ, path
 from typing import Any, Dict
 
 from dreamerv3 import configs, embodied
 
-from drl_air_hockey.utils.rewards import (
-    DefendReward,
-    HitReward,
-    PrepareReward,
-    TournamentReward,
-)
 from drl_air_hockey.utils.task import Task as AirHockeyTask
+from drl_air_hockey.utils.tournament_agent_strategies import (
+    strategy_from_str,
+    strategy_to_str,
+)
 
 ENV = AirHockeyTask.from_str(environ.get("AIR_HOCKEY_ENV", default="tournament"))
 
-REWARD_FUNCTION = None
-if ENV == AirHockeyTask.R7_TOURNAMENT:
-    REWARD_FUNCTION = TournamentReward()
-elif ENV == AirHockeyTask.R7_HIT:
-    REWARD_FUNCTION = HitReward()
-elif ENV == AirHockeyTask.R7_DEFEND:
-    REWARD_FUNCTION = DefendReward()
-elif ENV == AirHockeyTask.R7_PREPARE:
-    REWARD_FUNCTION = PrepareReward()
-else:
-    raise ValueError(f"Unknown environment name: {ENV}")
+AGENT_STRATEGY = strategy_from_str(
+    environ.get("AIR_HOCKEY_AGENT_STRATEGY", default="aggressive")
+)
 
 RENDER: bool = False
-EPISODE_MAX_STEPS: int = 3000
 INTERPOLATION_ORDER: int = -1
+
+EPISODE_MAX_STEPS: int = 45000
+MAX_TIME_UNTIL_PENALTY_S: int = 15.0
+
+DIR_MODELS: str = path.join(
+    path.abspath(path.dirname(path.dirname(__file__))),
+    "agents",
+    "models",
+)
 
 
 def config_dreamerv3(train: bool = False, preset: int = 2) -> Dict[str, Any]:
@@ -117,38 +115,141 @@ def config_dreamerv3(train: bool = False, preset: int = 2) -> Dict[str, Any]:
                 "disag_head.units": 256,
             }
         )
+    elif preset == 3:
+        config = config.update(
+            {
+                "logdir": path.join(
+                    path.dirname(path.dirname(path.abspath(path.dirname(__file__)))),
+                    "logdir_p3_" + ENV.to_str().lower().replace("7dof-", ""),
+                ),
+                "jax.platform": "cpu",
+                "jax.precision": "float32",
+                "jax.prealloc": True,
+                "imag_horizon": 50,
+                # encoder/decoder obs keys
+                "encoder.mlp_keys": "vector",
+                "decoder.mlp_keys": "vector",
+                # encoder
+                "encoder.mlp_layers": 2,
+                "encoder.mlp_units": 256,
+                # decoder
+                "decoder.mlp_layers": 2,
+                "decoder.mlp_units": 256,
+                # rssm
+                "rssm.deter": 512,
+                "rssm.units": 512,
+                "rssm.stoch": 32,
+                "rssm.classes": 32,
+                # actor
+                "actor.layers": 2,
+                "actor.units": 256,
+                # critic
+                "critic.layers": 2,
+                "critic.units": 512,
+                # reward
+                "reward_head.layers": 2,
+                "reward_head.units": 512,
+                # cont
+                "cont_head.layers": 2,
+                "cont_head.units": 512,
+                # disag
+                "disag_head.layers": 2,
+                "disag_head.units": 512,
+            }
+        )
+    elif preset == 4:
+        config = config.update(
+            {
+                "logdir": path.join(
+                    path.dirname(path.dirname(path.abspath(path.dirname(__file__)))),
+                    "logdir_p4_"
+                    + ENV.to_str().lower().replace("7dof-", "")
+                    + "_"
+                    + strategy_to_str(AGENT_STRATEGY),
+                ),
+                "jax.platform": "cpu",
+                "jax.precision": "float32",
+                "jax.prealloc": True,
+                "imag_horizon": 50,
+                # encoder/decoder obs keys
+                "encoder.mlp_keys": "vector",
+                "decoder.mlp_keys": "vector",
+                # encoder
+                "encoder.mlp_layers": 2,
+                "encoder.mlp_units": 256,
+                # decoder
+                "decoder.mlp_layers": 2,
+                "decoder.mlp_units": 256,
+                # rssm
+                "rssm.deter": 512,
+                "rssm.units": 512,
+                "rssm.stoch": 32,
+                "rssm.classes": 32,
+                # actor
+                "actor.layers": 2,
+                "actor.units": 512,
+                # critic
+                "critic.layers": 2,
+                "critic.units": 512,
+                # reward
+                "reward_head.layers": 2,
+                "reward_head.units": 512,
+                # cont
+                "cont_head.layers": 2,
+                "cont_head.units": 512,
+                # disag
+                "disag_head.layers": 2,
+                "disag_head.units": 512,
+            }
+        )
     else:
         raise ValueError(f"Unknown preset: {preset}")
 
     if train:
         # Configuration for a "large" machine
-        num_envs = 8
+        num_envs = 10
         config = config.update(
             {
                 "jax.platform": "gpu",
                 "envs.amount": num_envs,
                 "run.actor_batch": num_envs,
-                "replay_size": 2e6,
-                "run.steps": 5e7,
+                "replay_size": 1e7,
+                "run.steps": 2e8,
                 "run.log_every": 1024,
-                "run.train_ratio": 128,
+                "run.train_ratio": 384,
                 "batch_size": 32,
                 "batch_length": 64,
             }
         )
 
-        # # Configuration for a "small" machine
-        # num_envs = 4
+        # # Configuration for a "medium" machine
+        # num_envs = 8
         # config = config.update(
         #     {
         #         "jax.platform": "gpu",
         #         "envs.amount": num_envs,
         #         "run.actor_batch": num_envs,
-        #         "replay_size": 2e6,
-        #         "run.steps": 5e7,
+        #         "replay_size": 1e7,
+        #         "run.steps": 2e8,
         #         "run.log_every": 1024,
-        #         "run.train_ratio": 128,
+        #         "run.train_ratio": 512,
         #         "batch_size": 16,
+        #         "batch_length": 64,
+        #     }
+        # )
+
+        # # Configuration for a "small" machine
+        # num_envs = 5
+        # config = config.update(
+        #     {
+        #         "jax.platform": "gpu",
+        #         "envs.amount": num_envs,
+        #         "run.actor_batch": num_envs,
+        #         "replay_size": 1e7,
+        #         "run.steps": 2e8,
+        #         "run.log_every": 1024,
+        #         "run.train_ratio": 384,
+        #         "batch_size": 12,
         #         "batch_length": 64,
         #     }
         # )
